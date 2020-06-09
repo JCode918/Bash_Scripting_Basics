88354b7ca77248e34c36f2bbbc2c41784cee7a43
JCode918 <Willard.Joseph26@gmail.com>
2020-06-08 22:19:20 -0400

    First Commit

---
+++ HelloWorld.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+variable="Hello World"
+
+echo $variable
---
+++ QuestionResponse.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Question and Response With the Computer
+
+echo Hello What is your Name?
+read userName
+echo It is a pleasure to meet you $userName! Take Care!
+
+
